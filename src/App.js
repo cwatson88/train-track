8042fb4b040adf7edf0da71004d497a5b3ba1b37
Chris Watson <cwatson1988@gmail.com>
2018-05-31 18:26:28 +0100

    basic layout of the app has been created, next to add any dependency files. Readme has the outline plan.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
 
 class App extends Component {
+  
+  state = {
+    userPreferences: {
+      daysOfTravel: [
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday"
+      ],
+      hoursOfTravel: {
+        start: "0900",
+        stop: "1900"
+      }
+    },
+    journeys: [
+      {
+        departure: {
+          station: "London Euston",
+          time: "17:30"
+        },
+        arrival: {
+          station: "Birmingham International",
+          time: "18:45"
+        },
+        status: "on time"
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Welcome to Train Pain...
+            <span role="img" aria-label="bumped head emoji">
+              &#x1F631;
+            </span>
+            <span role="img" aria-label="train emoji">
+              &#x1F686;
+            </span>
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Fragment>
+          {
+            this.state.journeys.map(
+            (journey) => (
+              <div> { journey.departure.time } </div>
+            ) 
+          )}
+        </Fragment>
       </div>
     );
   }
